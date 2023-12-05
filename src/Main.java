75cd213213cec79c2fe64f130bc79c160414c6e2
user <danilovsansan@mail.ru>
2023-12-05 18:31:59 +0300

    дз 3/2 с комментариями

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,6 @@ public class Main {
         long integerLong = 8L;
         float fractionalFloat = 4f;
         double fractionalDouble = 8.88888;
-
-
         System.out.println(integerInt) ;
         System.out.println(integerByte );
         System.out.println(integerShort);
@@ -24,31 +22,26 @@ public class Main {
         System.out.println(fractionalFloat);
         System.out.println(fractionalDouble);
 
+        /*Задача 2
+        Ниже дан список различных значений. Инициализируйте переменные, используйте изученные ранее типы переменных.
+        Значения:
+            27.12
+            987 678 965 549
+            2,786
+            569
+            -159
+            27897
+            67
+        */
+        float a = 27.12f;
+               int b =-159;
+               int g =27897;
+                        double c = 2.786;
+                                byte d = 67;
+                                        short e = 569;
+                                                long f = 987678965549L;
 
 
-
-/*
-Подсказка
-
-
-
-
-Примеры инициализации переменных
-int a = 40000;
-
-byte b = 22;
-
-float с = 3.75f;
-
-
-
-Критерии оценки
- Объявили типы переменных int, byte, short, long, float, double.
- Назвали переменные согласно правилам Java.
- Соблюдаются правила пунктуации.
- Вывели значения всех переменных в консоль.
-
-*/
 
     }
 }
