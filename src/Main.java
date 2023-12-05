975bc9ce74741059c60e13526b25799489240868
user <danilovsansan@mail.ru>
2023-12-05 17:36:31 +0300

    дз 3/1 с коментариями

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args) {
+/*
+
+
+Задача 1
+Объявите переменные типа int, byte, short, long, float, double.
+Название переменных может быть любым, но если состоит из двух слов и более, должно соответствовать правилу camelCase.
+Выведите в консоль значение каждой переменной в формате «Значение переменной … с типом … равно …».
+*/
+
+        int integerInt = 4;
+        byte integerByte = 1;
+        short integerShort = 2;
+        long integerLong = 8L;
+        float fractionalFloat = 4f;
+        double fractionalDouble = 8.88888;
+
+
+        System.out.println(integerInt) ;
+        System.out.println(integerByte );
+        System.out.println(integerShort);
+        System.out.println(integerLong );
+        System.out.println(fractionalFloat);
+        System.out.println(fractionalDouble);
+
+
+
+
+/*
+Подсказка
+
+
+
+
+Примеры инициализации переменных
+int a = 40000;
+
+byte b = 22;
+
+float с = 3.75f;
+
+
+
+Критерии оценки
+ Объявили типы переменных int, byte, short, long, float, double.
+ Назвали переменные согласно правилам Java.
+ Соблюдаются правила пунктуации.
+ Вывели значения всех переменных в консоль.
+
+*/
+
+    }
+}
